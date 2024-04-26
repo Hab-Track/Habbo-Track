3585328e809e1b9dde7fe61223fbbeb77c0005dd
Lande <64076502+Laande@users.noreply.github.com>
2024-04-26 17:56:48 +0000

    keep furni script for maybe later

---
+++ src/command/furnitures.js
@@ -0,0 +1,39 @@
+const { fetchMany, fetchJson, config } = require('../functions')
+
+async function parse (json) {
+  const all = [
+    ...json.roomitemtypes.furnitype,
+    ...json.wallitemtypes.furnitype,
+  ]
+
+//   Download only swf
+  return new Set(
+    all.map((item) => {
+      return { revision: item.revision, name: `${item.classname.split('*')[0]}.swf` }
+    })
+  )
+
+// Download swf & icon
+//   all.forEach((item) => {
+//     map.push(
+//       { revision: item.revision, name: `${item.classname.replace('*', '_')}_icon.png` },
+//       { revision: item.revision, name: `${item.classname.split('*')[0]}.swf` },
+//     )
+//   })
+
+//   return new Set(map)
+}
+
+async function handle () {
+  const json = await fetchJson(`https://www.habbo.${config.domain}/gamedata/furnidata_json/0`)
+  const all = await parse(json)
+
+  await fetchMany([...all].map((item) => {
+    return {
+      src: `https://images.habbo.com/dcr/hof_furni/${item.revision}/${item.name}`,
+      dst: `furnis/${item.name}`
+    }
+  }))
+}
+
+module.exports = handle
