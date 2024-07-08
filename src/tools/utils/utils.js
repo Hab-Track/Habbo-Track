8fc0abc89f7c1acedcec744d692d65a49dd30a30
WiredL <retrobdersleri@gmail.com>
2024-07-08 22:14:46 +0300

    added send_vars

--- src/tools/utils/utils.js
+++ src/tools/utils/utils.js
@@ -39,14 +39,60 @@ function getCommitDetails(commitSha) {
       badges: 'Badge',
       clothes: 'Clothe',
       effects: 'Effect',
-      furnis: 'Furni'
+      furnis: 'Furni',
+      gamedata: 'Vars'
     }
   
     const parts = filePath.includes('/') ? filePath.split('/') : filePath.split('\\')
     const domain = domains.filter((d) => parts.includes(d)).pop()
     const type = types[Object.keys(types).filter((type) => parts.includes(type)).pop()]
-    const name = path.basename(filePath).substring(0, path.basename(filePath).lastIndexOf('.'))
+    const name = type === 'Vars' ? path.basename(filePath) : path.basename(filePath).substring(0, path.basename(filePath).lastIndexOf('.'))
     return domain ? `> ${type} (${domain}): ${name}` : `> ${type}: ${name}` 
   }
 
-  module.exports = { getCommitDetails, getBranchName, getUserAvatar, getLastCommitFiles, isImage, formatName }
+  function getCommitLines(commitSha) {
+    return execSync(`git show ${commitSha}`)
+    .toString()
+    .trim()
+    .split('\n')
+  }
+
+function generateDiscordDiffMessages(commitSha) {
+      const fileToMessagesMap = new Map()
+      const characterLimit = 1800 // max character amount in a single message
+      let currentFile
+      let message
+      const commitLines = getCommitLines(commitSha)
+      for (const line of commitLines) {
+          const match = line.match(/diff --git a\/([^ ]+)/)
+          if (match) {
+              if (message?.length > 7) {
+                  message += '\n```'
+                  fileToMessagesMap.get(currentFile).push(message)
+              }
+              
+              fileToMessagesMap.set(match[1], [])
+              currentFile = match[1]
+              message = '```diff'
+          }
+
+          if ((line.startsWith('+') || line.startsWith('-')) && !(line.startsWith('+++') || line.startsWith('---'))) {
+              message += `\n${line}`
+
+              if (message.length >= characterLimit) {
+                  message += '\n```'
+                  fileToMessagesMap.get(currentFile).push(message)
+                  message = '```diff'
+              }
+          }
+      }
+
+      if (message?.length > 7) {
+        message += '\n```'
+        fileToMessagesMap.get(currentFile).push(message)
+      }
+
+      return fileToMessagesMap
+}
+
+  module.exports = { getCommitDetails, getBranchName, getUserAvatar, getLastCommitFiles, isImage, formatName, generateDiscordDiffMessages }
